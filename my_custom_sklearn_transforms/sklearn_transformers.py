72cb28d808e6616748cfa09eed76abb8f2756e48
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2020-08-21 22:27:28 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,6 +1,4 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from xgboost import XGBClassifier
-from sklearn import model_selection
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
